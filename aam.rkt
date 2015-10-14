71d03723bf84b6632a8234954fc8daaf00997faa
Rebecca Chen <rchen@college.harvard.edu>
2015-10-14 13:27:34 -0400

    time-stamped CESK* machine
    Signed-off-by: Rebecca Chen <rchen@college.harvard.edu>

--- aam.rkt
+++ aam.rkt
@@ -1,11 +1,12 @@
 #lang racket
 (require redex)
-(define-language CESK*
+(define-language t-CESK*
   (x variable-not-otherwise-mentioned)
   (v (lmd x e)
      number)
-  (a number)
   (op * / + -)
+  (a number)
+  (t number)
   (e x
      v
      (e e)
@@ -19,38 +20,40 @@
      (op (vp ...) (c ...) a))
   (z vp k)
   (s ((a z) ...)))
-(define-metafunction CESK*
-  [(inj e) ((e ()) ((0 mt)) 0)])
-(define-metafunction CESK*
+(define-metafunction t-CESK*
+  [(inj e) ((e ()) ((0 mt)) 0 1)])
+(define-metafunction t-CESK*
   [(dlt * number ...) ,(apply * (term (number ...)))]
   [(dlt / number ...) ,(apply / (term (number ...)))]
   [(dlt + number ...) ,(apply + (term (number ...)))]
   [(dlt - number ...) ,(apply - (term (number ...)))])
-(define-metafunction CESK*
+(define-metafunction t-CESK*
   [(lookup x p) ,(second (findf (lambda (obj) (equal? (first obj) (term x))) (term p)))]
   [(lookup a s) ,(second (findf (lambda (obj) (equal? (first obj) (term a))) (term s)))])
-(define-metafunction CESK*
+(define-metafunction t-CESK*
   [(insert x a p) ,(cons (term (x a)) (term p))]
   [(insert a z s) ,(cons (term (a z)) (term s))])
-(define-metafunction CESK*
-  [(alloc s) ,(length (term s))])
+(define-metafunction t-CESK*
+  [(tick t) ,(+ (term t) 1)])
+(define-metafunction t-CESK*
+  [(alloc t) t])
 (define reduce
   (reduction-relation
-   CESK*
-   (--> ((x p) s a) ((lookup (lookup x p) s) s a))
-   (--> (((e_1 e_2) p) s a) ((e_1 p) (insert (alloc s) (arg (e_2 p) a) s) (alloc s)))
-   (--> (vp ((a_1 z_1) ... (a_3 (arg c a_4)) (a_2 z_2) ...) a_3)
+   t-CESK*
+   (--> ((x p) s a t) ((lookup (lookup x p) s) s a (tick t)))
+   (--> (((e_1 e_2) p) s a t) ((e_1 p) (insert (alloc t) (arg (e_2 p) a) s) (alloc t) (tick t)))
+   (--> (vp ((a_1 z_1) ... (a_3 (arg c a_4)) (a_2 z_2) ...) a_3 t)
         ,(let ([s (term ((a_1 z_1) ... (a_3 (arg c a_4)) (a_2 z_2) ...))])
-           (term (c (insert (alloc ,s) (fun vp a_4) ,s) (alloc ,s)))))
-   (--> (vp ((a_1 z_1) ... (a_3 (fun ((lmd x e) p) a_4)) (a_2 z_2) ...) a_3)
+           (term (c (insert (alloc t) (fun vp a_4) ,s) (alloc t) (tick t)))))
+   (--> (vp ((a_1 z_1) ... (a_3 (fun ((lmd x e) p) a_4)) (a_2 z_2) ...) a_3 t)
         ,(let ([s (term ((a_1 z_1) ... (a_3 (fun ((lmd x e) p) a_4)) (a_2 z_2) ...))])
-           (term ((e (insert x (alloc ,s) p)) (insert (alloc ,s) vp ,s) a_4))))
-   (--> (((op e_1 e_2 ...) p) s a)
+           (term ((e (insert x (alloc t) p)) (insert (alloc t) vp ,s) a_4 (tick t)))))
+   (--> (((op e_1 e_2 ...) p) s a t)
         ,(let ([cs (map (lambda (e) (list e (term p))) (term (e_2 ...)))])
-           (term ((e_1 p) (insert (alloc s) (op () ,cs a) s) (alloc s)))))
-   (--> (vp_1 ((a_1 z_1) ... (a_3 (op (vp_2 ...) (c_1 c_2 ...) a_4)) (a_2 z_2) ...) a_3)
+           (term ((e_1 p) (insert (alloc t) (op () ,cs a) s) (alloc t) (tick t)))))
+   (--> (vp_1 ((a_1 z_1) ... (a_3 (op (vp_2 ...) (c_1 c_2 ...) a_4)) (a_2 z_2) ...) a_3 t)
         ,(let ([s (term ((a_1 z_1) ... (a_3 (op (vp_2 ...) (c_1 c_2 ...) a_4)) (a_2 z_2) ...))])
-           (term (c_1 (insert (alloc ,s) (op (vp_2 ... vp_1) (c_2 ...) a_4) ,s) (alloc ,s)))))
-   (--> ((v_1 p_1) ((a_1 z_1) ... (a_3 (op ((v_2 p_2) ...) () a_4)) (a_2 z_2) ...) a_3)
+           (term (c_1 (insert (alloc t) (op (vp_2 ... vp_1) (c_2 ...) a_4) ,s) (alloc t) (tick t)))))
+   (--> ((v_1 p_1) ((a_1 z_1) ... (a_3 (op ((v_2 p_2) ...) () a_4)) (a_2 z_2) ...) a_3 t)
         ,(let ([s (term ((a_1 z_1) ... (a_3 (op ((v_2 p_2) ...) () a_4)) (a_2 z_2) ...))])
-           (term (((dlt op v_2 ... v_1) ()) ,s a_4))))))
+           (term (((dlt op v_2 ... v_1) ()) ,s a_4 (tick t)))))))
