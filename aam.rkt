4e5a59a28f2a9129d80efa3c9f25eb402c2bbd4d
Rebecca Chen <rchen@college.harvard.edu>
2015-10-21 04:21:45 -0400

    Add escape information.
    Signed-off-by: Rebecca Chen <rchen@college.harvard.edu>

--- aam.rkt
+++ aam.rkt
@@ -1,59 +1,65 @@
 #lang racket
 (require redex)
-(define-language t-CESK*
-  (x variable-not-otherwise-mentioned)
-  (v (lmd x e)
-     number)
-  (op * / + -)
-  (a number)
-  (t number)
+(define-language et-CESK*
   (e x
      v
      (e e)
      (op e ...))
-  (vp (v p))
-  (c (e p))
-  (p ((x a) ...))
+  (v (lmd x e)
+     number)
+  (x variable-not-otherwise-mentioned)
+  (op * / + -)
+  (c (e r a))
+  (r ((x a) ...))
+  (s ((a vk) ...))
   (k mt
-     (arg c a)
-     (fun vp a)
-     (op (vp ...) (c ...) a))
-  (z vp k)
-  (s ((a z) ...)))
-(define-metafunction t-CESK*
-  [(inj e) ((e ()) ((0 mt)) 0 1)])
-(define-metafunction t-CESK*
+     (ar c aa)
+     (fn vr aa)
+     (op (vr ...) (c ...) aa))
+  (a number)
+  (t number)
+  (C (c ...))
+  (vk vr k)
+  (aa (a a))
+  (vr (v r a)))
+(define-metafunction et-CESK*
+  [(inj e) ((e () 0) ((0 mt)) (0 0) 0 ())])
+(define-metafunction et-CESK*
   [(dlt * number ...) ,(apply * (term (number ...)))]
   [(dlt / number ...) ,(apply / (term (number ...)))]
   [(dlt + number ...) ,(apply + (term (number ...)))]
   [(dlt - number ...) ,(apply - (term (number ...)))])
-(define-metafunction t-CESK*
-  [(lookup x p) ,(second (findf (lambda (obj) (equal? (first obj) (term x))) (term p)))]
-  [(lookup a s) ,(second (findf (lambda (obj) (equal? (first obj) (term a))) (term s)))])
-(define-metafunction t-CESK*
-  [(insert x a p) ,(cons (term (x a)) (term p))]
-  [(insert a z s) ,(cons (term (a z)) (term s))])
-(define-metafunction t-CESK*
+(define-metafunction et-CESK*
+  [(lookup x r) ,(second (assoc (term x) (term r)))]
+  [(lookup a s) ,(second (assoc (term a) (term s)))])
+(define-metafunction et-CESK*
+  [(insert x a r) ,(cons (term (x a)) (term r))]
+  [(insert a vk s) ,(cons (term (a vk)) (term s))]
+  [(insert (e r a) s C) ,(if (or (assoc (term a) (term s)) (member (term (e r a)) (term C))) (term C) (cons (term (e r a)) (term C)))]
+  [(insert (c ...) s C) ,(foldl (lambda (elem acc) (term (insert ,elem s ,acc))) (term C) (term (c ...)))])
+(define-metafunction et-CESK*
   [(tick t) ,(+ (term t) 1)])
-(define-metafunction t-CESK*
+(define-metafunction et-CESK*
   [(alloc t) t])
 (define reduce
   (reduction-relation
-   t-CESK*
-   (--> ((x p) s a t) ((lookup (lookup x p) s) s a (tick t)))
-   (--> (((e_1 e_2) p) s a t) ((e_1 p) (insert (alloc t) (arg (e_2 p) a) s) (alloc t) (tick t)))
-   (--> (vp ((a_1 z_1) ... (a_3 (arg c a_4)) (a_2 z_2) ...) a_3 t)
-        ,(let ([s (term ((a_1 z_1) ... (a_3 (arg c a_4)) (a_2 z_2) ...))])
-           (term (c (insert (alloc t) (fun vp a_4) ,s) (alloc t) (tick t)))))
-   (--> (vp ((a_1 z_1) ... (a_3 (fun ((lmd x e) p) a_4)) (a_2 z_2) ...) a_3 t)
-        ,(let ([s (term ((a_1 z_1) ... (a_3 (fun ((lmd x e) p) a_4)) (a_2 z_2) ...))])
-           (term ((e (insert x (alloc t) p)) (insert (alloc t) vp ,s) a_4 (tick t)))))
-   (--> (((op e_1 e_2 ...) p) s a t)
-        ,(let ([cs (map (lambda (e) (list e (term p))) (term (e_2 ...)))])
-           (term ((e_1 p) (insert (alloc t) (op () ,cs a) s) (alloc t) (tick t)))))
-   (--> (vp_1 ((a_1 z_1) ... (a_3 (op (vp_2 ...) (c_1 c_2 ...) a_4)) (a_2 z_2) ...) a_3 t)
-        ,(let ([s (term ((a_1 z_1) ... (a_3 (op (vp_2 ...) (c_1 c_2 ...) a_4)) (a_2 z_2) ...))])
-           (term (c_1 (insert (alloc t) (op (vp_2 ... vp_1) (c_2 ...) a_4) ,s) (alloc t) (tick t)))))
-   (--> ((v_1 p_1) ((a_1 z_1) ... (a_3 (op ((v_2 p_2) ...) () a_4)) (a_2 z_2) ...) a_3 t)
-        ,(let ([s (term ((a_1 z_1) ... (a_3 (op ((v_2 p_2) ...) () a_4)) (a_2 z_2) ...))])
-           (term (((dlt op v_2 ... v_1) ()) ,s a_4 (tick t)))))))
+   et-CESK*
+   (--> ((x r a_c) s aa t C) ((lookup (lookup x r) s) s aa (tick t) C))
+   (--> (((e_1 e_2) r a_c) s (a a_f) t C)
+        ,(let* ([t_p (term (tick t))] [a_p (term (alloc ,t_p))])
+           (term ((e_1 r a_f) (insert ,a_p (ar (e_2 r a_f) (a a_f)) s) (,a_p a_f) ,t_p (insert ((e_1 e_2) r a_c) s C)))))
+   (--> (((op e_1 e_2 ...) r a_c) s (a a_f) t C)
+        ,(let* ([t_p (term (tick t))] [a_p (term (alloc ,t_p))] [cs (map (lambda (e) (list e (term r) (term a_f))) (term (e_2 ...)))])
+           (term ((e_1 r a_f) (insert ,a_p (op () ,cs (a a_f)) s) (,a_p a_f) ,t_p (insert ((op e_1 e_2 ...) r a_c) s C)))))
+   (--> (vr ((a_1 vk_1) ... (a (ar c aa_pp)) (a_2 vk_2) ...) (a a_f) t C)
+        ,(let* ([t_p (term (tick t))] [a_p (term (alloc ,t_p))] [s (term ((a_1 vk_1) ... (a (ar c aa_pp)) (a_2 vk_2) ...))])
+           (term (c (insert ,a_p (fn vr aa_pp) ,s) (,a_p ,a_p) ,t_p C))))
+   (--> (vr ((a_1 vk_1) ... (a (fn ((lmd x e) r a_c) aa_pp)) (a_2 vk_2) ...) (a a_f) t C)
+        ,(let* ([t_p (term (tick t))] [a_p (term (alloc ,t_p))] [s (term ((a_1 vk_1) ... (a (fn ((lmd x e) r a_c) aa_pp)) (a_2 vk_2) ...))])
+           (term ((e (insert x ,a_p r) a_f) (insert ,a_p vr ((a_1 vk_1) ... (a_2 vk_2) ...)) aa_pp ,t_p (insert ((lmd x e) r a_c) ,s C)))))
+   (--> (vr ((a_1 vk_1) ... (a (op (vr_1 ...) (c_1 c_2 ...) aa_pp)) (a_2 vk_2) ...) (a a_f) t C)
+        ,(let* ([t_p (term (tick t))] [a_p (term (alloc ,t_p))] [s (term ((a_1 vk_1) ... (a (op (vr_1 ...) (c_1 c_2 ...) aa_pp)) (a_2 vk_2) ...))])
+           (term (c_1 (insert ,a_p (op (vr_1 ... vr) (c_2 ...) aa_pp) ,s) (,a_p a_f) ,t_p C))))
+   (--> ((v r_n a_n) ((a_1 vk_1) ... (a (op ((v_1 r_n1 a_n1) ...) () aa_pp)) (a_2 vk_2) ...) (a a_f) t C)
+        ,(let* ([t_p (term (tick t))] [a_p (term (alloc ,t_p))] [s (term ((a_1 vk_1) ... (a (op ((v_1 r_n1 a_n1) ...) () aa_pp)) (a_2 vk_2) ...))])
+           (term (((dlt op v_1 ... v) () a_f) ,s aa_pp ,t_p (insert ((v_1 r_n1 a_n1) ... (v r_n a_n)) ,s C)))))))
