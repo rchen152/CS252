433a637ef5162668d66963786dedbb2db57ed0d1
Rebecca Chen <rchen@college.harvard.edu>
2015-10-28 11:45:55 -0400

    Remove integers, flatten state.
    Signed-off-by: Rebecca Chen <rchen@college.harvard.edu>

--- aam.rkt
+++ aam.rkt
@@ -3,23 +3,16 @@
 (define-language tCESK*
   (e x
      v
-     (e e)
-     (op e ...))
-  (v (lmd x e)
-     number)
+     (e e))
+  (v (lmd x e))
   (x variable-not-otherwise-mentioned)
-  (op * / + -)
-  (c (e r))
   (r any)
   (s any)
   (k mt
-     (ar c a)
-     (fn vr a)
-     (op (vr ...) (c ...) a))
+     (ar e r a)
+     (fn v r a))
   (a number)
-  (t number)
-  (vk vr k)
-  (vr (v r)))
+  (t number))
 (define-metafunction tCESK*
   [(empty) ,(hash)])
 (define-metafunction tCESK*
@@ -27,12 +20,7 @@
 (define-metafunction tCESK*
   [(insert any_key any_val any_map) ,(hash-set (term any_map) (term any_key) (term any_val))])
 (define-metafunction tCESK*
-  [(inj e) ((e (empty)) (insert 0 mt (empty)) 0 1)])
-(define-metafunction tCESK*
-  [(dlt * number ...) ,(apply * (term (number ...)))]
-  [(dlt / number ...) ,(apply / (term (number ...)))]
-  [(dlt + number ...) ,(apply + (term (number ...)))]
-  [(dlt - number ...) ,(apply - (term (number ...)))])
+  [(inj e) (e (empty) (insert 0 mt (empty)) 0 1)])
 (define-metafunction tCESK*
   [(tick t) ,(+ (term t) 1)])
 (define-metafunction tCESK*
@@ -40,26 +28,15 @@
 (define reduce
   (reduction-relation
    tCESK*
-   (--> ((x r) s a t) ((lookup (lookup x r) s) s a (tick t)) "1")
-   (--> (((e_1 e_2) r) s a t)
+   (--> (x r s a t) ,(append (term (lookup (lookup x r) s)) (term (s a (tick t)))) "1")
+   (--> ((e_1 e_2) r s a t)
         ,(let ([t_p (term (tick t))] [a_p (term (alloc t))])
-           (term ((e_1 r) (insert ,a_p (ar (e_2 r) a) s) ,a_p ,t_p))) "2")
-   (--> (((op e_1 e_2 ...) r) s a t)
-        ,(let ([t_p (term (tick t))] [a_p (term (alloc t))] [cs (map (lambda (e) (list e (term r))) (term (e_2 ...)))])
-           (term ((e_1 r) (insert ,a_p (op () ,cs a) s) ,a_p ,t_p))) "3")
-   (--> (vr s a t)
+           (term (e_1 r (insert ,a_p (ar e_2 r a) s) ,a_p ,t_p))) "2")
+   (--> (v r s a t)
         ,(let ([t_p (term (tick t))] [a_p (term (alloc t))])
-           (term (c (insert ,a_p (fn vr a_pp) s) ,a_p ,t_p)))
-        (where (ar c a_pp) (lookup a s)) "4")
-   (--> (vr s a t)
+           (term (e r_p (insert ,a_p (fn v r a_pp) s) ,a_p ,t_p)))
+        (where (ar e r_p a_pp) (lookup a s)) "3")
+   (--> (v r s a t)
         ,(let ([t_p (term (tick t))] [a_p (term (alloc t))])
-           (term ((e (insert x ,a_p r)) (insert ,a_p vr s) a_pp ,t_p)))
-        (where (fn ((lmd x e) r) a_pp) (lookup a s)) "5")
-   (--> (vr s a t)
-        ,(let ([t_p (term (tick t))] [a_p (term (alloc t))])
-           (term (c_1 (insert ,a_p (op (vr_1 ... vr) (c_2 ...) a_pp) s) ,a_p ,t_p)))
-        (where (op (vr_1 ...) (c_1 c_2 ...) a_pp) (lookup a s)) "6")
-   (--> ((v r_n) s a t)
-        ,(let ([t_p (term (tick t))] [a_p (term (alloc t))])
-           (term (((dlt op v_1 ... v) (empty)) s a_pp ,t_p)))
-        (where (op ((v_1 r_n1) ...) () a_pp) (lookup a s)) "7")))
+           (term (e (insert x ,a_p r_p) (insert ,a_p (v r) s) a_pp ,t_p)))
+        (where (fn (lmd x e) r_p a_pp) (lookup a s)) "4")))
